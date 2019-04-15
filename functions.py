da5476f0f6377c8feb3e8cfbe66edb911e073182
vagrant <vagrant@vagrant.vm>
2019-04-15 00:39:45 +0000

    first_version

--- functions.py
+++ functions.py
@@ -14,42 +14,97 @@ go below this docstring.
 
    # 1.  Write a function that takes a town name as a string and returns
    #     `True` if it is your hometown, and `False` otherwise.
+def print_town_name(hometown):
+   return  hometown == "Chicago"
 
-   # 2.  Write a function that takes a first and last name as arguments and
-   #     returns the concatenation of the two names in one string.
+print(print_town_name("a"))
 
-   # 3.  Write a function that takes a home town, a first name, and a last name
-   #     as arguments, calls both functions from part (a) and (b) and prints
-   #     "Hi, 'full name here', we're from the same place!", or "Hi 'full name
-   #     here', I'd like to visit 'town name here'!" depending on what the function
-   #     from part (a) evaluates to.
 
-   # 4.  Write a function, `is_berry()`, which takes a fruit name as a string
-   #     and returns a boolean if the fruit is a "strawberry", "raspberry",
-   #     "blackberry", or "currant."
 
-   # 5.  Write another function, shipping_cost(), which calculates shipping
-   #     cost by taking a fruit name as a string and calling the `is_berry()`
-   #     function within the `shipping_cost()` function. Your function should
-   #     return 0 if is_berry() == True, and 5 if is_berry() == False.
+# 2.  Write a function that takes a first and last name as arguments and
+#     returns the concatenation of the two names in one string.
+def print_firstname_lastname(firstname,lastname):
+   return  f" My name is {firstname} {lastname}"
 
-   # 6.  Make a function that takes in a fruit name and a list of fruits. It should
-   #     return a new list containing the elements of the input list, along with
-   #     given fruit, which should be at the end of the new list.
+print(print_firstname_lastname("Judith", "T"))
 
-   # 7.  Write a function calculate_price to calculate an item's total cost by
-   #     adding tax and any fees required by state law.
+# 3.  Write a function that takes a home town, a first name, and a last name
+#     as arguments, calls both functions from part (a) and (b) and prints
+#     "Hi, 'full name here', we're from the same place!", or "Hi 'full name
+#     here', I'd like to visit 'town name here'!" depending on what the function
+#     from part (a) evaluates to.
+def print_person_introduction(firstname, lastname, hometown):
+   if print_town_name(hometown) == True:
+      return f"Hi, {firstname} {lastname} here, we're from the same place"
+   else:
+      return f"Hi, {firstname} {lastname} here, I'd like to visit {hometown}"
 
-   #     Your function will take as parameters (in this order): the base price of
-   #     the item, a two-letter state abbreviation, and the tax percentage (as a
-   #     two-digit decimal, so, for instance, 5% will be .05). If the user does not
-   #     provide a tax rate it should default to 5%.
+print(print_person_introduction("Miki", "ketcha", "Nyalla"))
+print(print_person_introduction("cedric", "ketcha", "Chicago"))
 
-   #     CA law requires stores to collect a 3% recycling fee, PA requires a $2
-   #     highway safety fee, and in MA, there is a Commonwealth Fund fee of $1 for
-   #     items with a base price of $100 or less and $3 for items over $100. Fees are
-   #     added *after* the tax is calculated.
+# 4.  Write a function, `is_berry()`, which takes a fruit name as a string
+#     and returns a boolean if the fruit is a "strawberry", "raspberry",
+#     "blackberry", or "currant."
+def is_berry(fruit):
+   if fruit == "strawberry" or fruit == "raspberry" or fruit == "blackberry" or fruit == "currant.":
+      return True
+   else:
+      return False
 
-   #     Your function should return the total cost of the item, including tax and
-   #     fees.
+print(is_berry("pineapple"))
+print(is_berry("blackberry"))
 
+# 5.  Write another function, shipping_cost(), which calculates shipping
+#     cost by taking a fruit name as a string and calling the `is_berry()`
+#     function within the `shipping_cost()` function. Your function should
+#     return 0 if is_berry() == True, and 5 if is_berry() == False.
+def shipping_cost(fruit):
+   if is_berry(fruit):
+      return 0
+   else:
+      return 5
+
+print(shipping_cost("banana"))
+
+
+# 6.  Make a function that takes in a fruit name and a list of fruits. It should
+#     return a new list containing the elements of the input list, along with
+#     given fruit, which should be at the end of the new list.
+
+def adding_a_new_fruit_to_list(fruit,list):
+    list.append(fruit)
+    return list
+
+print(adding_a_new_fruit_to_list("banana", ["mango","pear","avocado"]))
+
+# 7.  Write a function calculate_price to calculate an item's total cost by
+#     adding tax and any fees required by state law.
+
+def calculate_price(base_price,state,tax=.05):
+   if state == "CA":
+      total_cost = base_price + base_price * .03
+   elif state == "PA":
+      total_cost = (base_price +  base_price * tax) + 2 
+   elif state == "MA":
+      if base_price < 100:
+         total_cost = (base_price + base_price * tax) + 1
+      else:
+         total_cost = (base_price + base_price * tax) + 3
+   return total_cost
+
+print(calculate_price(100, "CA"))
+print(calculate_price(100, "MA", .05))
+print(calculate_price(100, "PA"))
+
+#     Your function will take as parameters (in this order): the base price of
+#     the item, a two-letter state abbreviation, and the tax percentage (as a
+#     two-digit decimal, so, for instance, 5% will be .05). If the user does not
+#     provide a tax rate it should default to 5%.
+
+#     CA law requires stores to collect a 3% recycling fee, PA requires a $2
+#     highway safety fee, and in MA, there is a Commonwealth Fund fee of $1 for
+#     items with a base price of $100 or less and $3 for items over $100. Fees are
+#     added *after* the tax is calculated.
+
+#     Your function should return the total cost of the item, including tax and
+#     fees.
